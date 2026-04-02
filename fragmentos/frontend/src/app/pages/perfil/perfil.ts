52508d642bc002b7585f0b872e8ce72008e74a2c
Cat-sleepy <caty.vizela@gmail.com>
2026-04-02 23:45:16 +0100

    feat: submission form with file upload and profile page

--- fragmentos/frontend/src/app/pages/perfil/perfil.ts
+++ fragmentos/frontend/src/app/pages/perfil/perfil.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Fragment } from '../../services/fragment';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './perfil.html',
   styleUrl: './perfil.css',
 })
-export class Perfil {}
+export class Perfil implements OnInit {
+  fragments: any[] = [];
+  loading = true;
+  erro = '';
+
+  constructor(private fragmentService: Fragment) {}
+
+  ngOnInit() {
+    this.fragmentService.getMyFragments().subscribe({
+      next: (res) => {
+        this.fragments = res.data;
+        this.loading = false;
+      },
+      error: () => {
+        this.erro = 'Erro ao carregar os teus fragmentos.';
+        this.loading = false;
+      }
+    });
+  }
+
+  eliminar(id: number) {
+    this.fragmentService.deleteFragment(id).subscribe({
+      next: () => {
+        this.fragments = this.fragments.filter(f => f.id !== id);
+      },
+      error: () => {
+        this.erro = 'Erro ao eliminar fragmento.';
+      }
+    });
+  }
+}
